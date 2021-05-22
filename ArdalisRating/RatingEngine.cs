c614c7b2007b6b118a57a56509d7e869646041fb
Ricardo Brito Brens <britobrensricardo@gmail.com>
2021-05-21 20:31:36 -0400

    [IMP] second rafactoring to apply Open Closed Principle

--- ArdalisRating/RatingEngine.cs
+++ ArdalisRating/RatingEngine.cs
@@ -11,95 +11,42 @@ namespace ArdalisRating
     /// </summary>
     public class RatingEngine
     {
+        public ConsoleLogger Logger = new ConsoleLogger();
+        public FilePolicySource FilePolicySource = new FilePolicySource();
+        public JsonPolicyDeserialilzer PolicyDeserialilzer = new JsonPolicyDeserialilzer();
+
         public decimal Rating { get; set; }
         public void Rate()
         {
-            Console.WriteLine("Starting rate.");
+            Logger.Log("Starting rate.");
+            Logger.Log("Loading policy.");
 
-            Console.WriteLine("Loading policy.");
-
-            // load policy - open file policy.json
-            string policyJson = File.ReadAllText("policy.json");
-
-            var policy = JsonConvert.DeserializeObject<Policy>(policyJson,
-                new StringEnumConverter());
+            string policyJson = FilePolicySource.GetFilePolictyFromSource("policy.json");
+            var policy = PolicyDeserialilzer.GetDeserializePolicy(policyJson);
 
             switch (policy.Type)
             {
                 case PolicyType.Auto:
-                    Console.WriteLine("Rating AUTO policy...");
-                    Console.WriteLine("Validating policy.");
-                    if (String.IsNullOrEmpty(policy.Make))
-                    {
-                        Console.WriteLine("Auto policy must specify Make");
-                        return;
-                    }
-                    if (policy.Make == "BMW")
-                    {
-                        if (policy.Deductible < 500)
-                        {
-                            Rating = 1000m;
-                        }
-                        Rating = 900m;
-                    }
+                    var autoRater = new AutoPolicyRater(this, Logger);
+                    autoRater.Rate(policy);
                     break;
 
                 case PolicyType.Land:
-                    Console.WriteLine("Rating LAND policy...");
-                    Console.WriteLine("Validating policy.");
-                    if (policy.BondAmount == 0 || policy.Valuation == 0)
-                    {
-                        Console.WriteLine("Land policy must specify Bond Amount and Valuation.");
-                        return;
-                    }
-                    if (policy.BondAmount < 0.8m * policy.Valuation)
-                    {
-                        Console.WriteLine("Insufficient bond amount.");
-                        return;
-                    }
-                    Rating = policy.BondAmount * 0.05m;
+                    var landRater = new LandPolictyRater(this, Logger);
+                    landRater.Rate(policy);
                     break;
 
                 case PolicyType.Life:
-                    Console.WriteLine("Rating LIFE policy...");
-                    Console.WriteLine("Validating policy.");
-                    if (policy.DateOfBirth == DateTime.MinValue)
-                    {
-                        Console.WriteLine("Life policy must include Date of Birth.");
-                        return;
-                    }
-                    if (policy.DateOfBirth < DateTime.Today.AddYears(-100))
-                    {
-                        Console.WriteLine("Centenarians are not eligible for coverage.");
-                        return;
-                    }
-                    if (policy.Amount == 0)
-                    {
-                        Console.WriteLine("Life policy must include an Amount.");
-                        return;
-                    }
-                    int age = DateTime.Today.Year - policy.DateOfBirth.Year;
-                    if (policy.DateOfBirth.Month == DateTime.Today.Month &&
-                        DateTime.Today.Day < policy.DateOfBirth.Day ||
-                        DateTime.Today.Month < policy.DateOfBirth.Month)
-                    {
-                        age--;
-                    }
-                    decimal baseRate = policy.Amount * age / 200;
-                    if (policy.IsSmoker)
-                    {
-                        Rating = baseRate * 2;
-                        break;
-                    }
-                    Rating = baseRate;
+                    var lifeRater = new LifePolicyRater(this, Logger);
+                    lifeRater.Rate(policy);
                     break;
 
                 default:
-                    Console.WriteLine("Unknown policy type");
+                    Logger.Log("Unknown policy type");
                     break;
             }
 
-            Console.WriteLine("Rating completed.");
+            Logger.Log("Rating completed.");
         }
     }
 }
